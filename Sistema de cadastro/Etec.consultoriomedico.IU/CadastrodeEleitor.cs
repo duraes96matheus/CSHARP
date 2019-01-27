4f7416c93b0206258cbfb9ebd2f0a86c22208407
duraes96matheus <duraes96matheus@gmail.com>
2019-01-27 15:40:00 -0200

    Sistema de cadastro

---
+++ Sistema de cadastro/Etec.consultoriomedico.IU/CadastrodeEleitor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Etec.consultoriomedico.IU
+{
+    public partial class CadastrodeEleitor : Form
+    {
+        public CadastrodeEleitor()
+        {
+            InitializeComponent();
+        }
+
+        private void CadastrodeEleitor_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Sistema de cadastro/Etec.consultoriomedico.IU/Cadastrodecanditado.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Etec.consultoriomedico.IU
+{
+    public partial class CadastrodeCanditado : Form
+    {
+        public CadastrodeCanditado()
+        {
+            InitializeComponent();
+        }
+
+        private void CadastrodeCanditado_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
