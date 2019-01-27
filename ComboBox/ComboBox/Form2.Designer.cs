3c80e747c084423a46b06d7b679aa3f5f255f44c
duraes96matheus <duraes96matheus@gmail.com>
2019-01-27 15:36:41 -0200

    Exemplo de Combobox

---
+++ ComboBox/ComboBox/Form2.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboestados = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.combocidades = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(32, 194);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(14, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Estado";
+            // 
+            // comboestados
+            // 
+            this.comboestados.FormattingEnabled = true;
+            this.comboestados.Items.AddRange(new object[] {
+            "São Paulo",
+            "Rio de Janeiro",
+            "Minas Gerais",
+            "Bahia"});
+            this.comboestados.Location = new System.Drawing.Point(60, 30);
+            this.comboestados.Name = "comboestados";
+            this.comboestados.Size = new System.Drawing.Size(121, 21);
+            this.comboestados.TabIndex = 2;
+            this.comboestados.SelectedIndexChanged += new System.EventHandler(this.comboestados_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(14, 70);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Cidade";
+            // 
+            // combocidades
+            // 
+            this.combocidades.FormattingEnabled = true;
+            this.combocidades.ImeMode = System.Windows.Forms.ImeMode.Disable;
+            this.combocidades.Location = new System.Drawing.Point(60, 67);
+            this.combocidades.Name = "combocidades";
+            this.combocidades.Size = new System.Drawing.Size(121, 21);
+            this.combocidades.TabIndex = 2;
+            this.combocidades.SelectedIndexChanged += new System.EventHandler(this.combocidades_SelectedIndexChanged);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.combocidades);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboestados);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboestados;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox combocidades;
+    }
+}
---
+++ ComboBox/ComboBox/Form2.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            carregarComboEstado();
+            carregarComboCidades();
+        }
+        private void carregarComboEstado()
+        {
+            SqlConnection conexao = new SqlConnection();
+            conexao.ConnectionString = Program.conexaoBD;
+            conexao.Open();
+
+
+            string comandoSQL = "SELECT * FROM Estados";
+
+            SqlCommand sqlCommand = new SqlCommand(comandoSQL, conexao);
+
+            SqlDataAdapter sda = new SqlDataAdapter(sqlCommand);
+
+            DataTable dtFuncionariosBD = new DataTable();
+
+            sda.Fill(dtFuncionariosBD);
+
+            comboestados.DataSource = dtFuncionariosBD;
+
+            comboestados.DisplayMember = "NomeEstado";
+
+
+
+            conexao.Close();
+        }
+
+        private void carregarComboCidades()
+        {
+            SqlConnection conexao = new SqlConnection();
+            conexao.ConnectionString = Program.conexaoBD;
+            conexao.Open();
+
+            string comandoSQL = "SELECT * FROM Cidade where ID_Estado='"ID_estado"';";
+
+            SqlCommand sqlCommand = new SqlCommand(comandoSQL, conexao);
+
+            SqlDataAdapter sda = new SqlDataAdapter(sqlCommand);
+
+            DataTable dtFuncionariosBD = new DataTable();
+
+            sda.Fill(dtFuncionariosBD);
+
+            combocidades.DataSource = dtFuncionariosBD;
+
+            combocidades.DisplayMember = "NomeCidade";
+
+
+
+            conexao.Close();
+        }
+
+        private void comboestados_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void combocidades_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+       
+  
+    }
+}
