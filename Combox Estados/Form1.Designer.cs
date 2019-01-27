e073952caee2e46473231a2bedee003029b48e16
duraes96matheus <duraes96matheus@gmail.com>
2019-01-27 15:44:14 -0200

    Exemplo de combobox

---
+++ Combox Estados/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace Estados.cs
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtEstado = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dgvestado = new System.Windows.Forms.DataGridView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.lblHabitantes = new System.Windows.Forms.Label();
+            this.btnsalvar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvestado)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(45, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nome do Estado";
+            // 
+            // txtEstado
+            // 
+            this.txtEstado.Location = new System.Drawing.Point(137, 10);
+            this.txtEstado.Name = "txtEstado";
+            this.txtEstado.Size = new System.Drawing.Size(185, 20);
+            this.txtEstado.TabIndex = 1;
+            this.txtEstado.TextChanged += new System.EventHandler(this.txtEstado_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(340, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(79, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "PESQUISAR";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dgvestado
+            // 
+            this.dgvestado.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvestado.Location = new System.Drawing.Point(12, 71);
+            this.dgvestado.Name = "dgvestado";
+            this.dgvestado.Size = new System.Drawing.Size(407, 319);
+            this.dgvestado.TabIndex = 3;
+            this.dgvestado.CellContentDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvestado_CellContentDoubleClick);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(474, 71);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // lblHabitantes
+            // 
+            this.lblHabitantes.AutoSize = true;
+            this.lblHabitantes.Location = new System.Drawing.Point(471, 42);
+            this.lblHabitantes.Name = "lblHabitantes";
+            this.lblHabitantes.Size = new System.Drawing.Size(58, 13);
+            this.lblHabitantes.TabIndex = 5;
+            this.lblHabitantes.Text = "Habitantes";
+            // 
+            // btnsalvar
+            // 
+            this.btnsalvar.Location = new System.Drawing.Point(488, 97);
+            this.btnsalvar.Name = "btnsalvar";
+            this.btnsalvar.Size = new System.Drawing.Size(75, 23);
+            this.btnsalvar.TabIndex = 6;
+            this.btnsalvar.Text = "Salvar";
+            this.btnsalvar.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(641, 420);
+            this.Controls.Add(this.btnsalvar);
+            this.Controls.Add(this.lblHabitantes);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dgvestado);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtEstado);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvestado)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtEstado;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dgvestado;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label lblHabitantes;
+        private System.Windows.Forms.Button btnsalvar;
+    }
+}
+
---
+++ Combox Estados/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Estados.cs
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void carregarEstados()
+        {
+            
+            DataSet ds = new DataSet();
+            SqlConnection conexao = null;
+
+            try
+            {
+                conexao = new SqlConnection("Password=info211;Persist Security Info=True;User ID=sa;Initial Catalog=Cidade;Data Source=LAB-08-20");
+                conexao.Open();
+                SqlCommand cmd = new SqlCommand("sp_RetornaEstados", conexao);
+
+
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                if (txtEstado.Text != "")
+                {
+                    cmd.Parameters.AddWithValue("@nmEstado", txtEstado.Text);
+                }
+
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+
+                da.Fill(ds);
+
+                dgvestado.DataSource = ds.Tables[0]; 
+
+
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show("Contate o Administrador Agora FDp!!!!");
+            }
+            finally
+            {
+                conexao.Close();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            carregarEstados();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dgvestado.DataSource = null;
+            carregarEstados();
+            
+        }
+
+        private void txtEstado_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dgvestado_CellContentDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
